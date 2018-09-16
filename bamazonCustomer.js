64e69b5f361e0cc244020d46d139637842c7afd8
Matthew Huberty <matthew.huberty@gmail.com>
2018-09-16 11:41:11 -0700

    Sep16_v3

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,31 +17,28 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    //console.log("Connected as ID:  " + connection.threadId + "\n");
     start();
-    displayItems();
-    //Call a variety of functions, depending upon what you want to do
-    //runSearch();
-    //createProduct();
-    //start();
 });
 
 function start() {
+    //Call the displayItems function, so as to display all items for sale.
+    displayItems();
     var questions = {
-        name: "idOrQuantity",
+        name: "purchase",
         type: "rawlist",
-        message: "Would you like to enter an [ID] or [QUANTITY] for an item to purchase or exit the program? [TYPE 1, 2, OR 3]",
-        choices: ["ID", "QUANTITY", "EXIT"]
+        message: "Would you like to purchase an item or exit the program? [TYPE 1, OR 2]",
+        choices: ["PURCHASE", "EXIT"]
     };
 
     inquirer.prompt(questions).then(function (answer) {
         //based on the user's answer, either enter the ID or quantity of the item they'd like to purchase
-        if (answer.idOrQuantity.toUpperCase() === "ID") {
-            idAction();
+        if (answer.purchase.toUpperCase() === "PURCHASE") {
+            purchase();
+            return;
         }
-        else if (answer.idOrQuantity.toUpperCase() === "POST") {
-            postAction();
-        }
+        // else if (answer.idOrQuantity.toUpperCase() === "QUANTITY") {
+        //     postAction();
+        // }
         else {
             //Exit the application
             connection.end();
@@ -50,44 +47,74 @@ function start() {
     });
 }
 
-function runSearch() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "Enter the ID of the product I wish to purchase",
-                "Enter the number of units of the product I'd like to buy"
-            ]
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "Enter the ID of the product I wish to purchase":
-                    artistSearch();
-                    break;
-
-                case "Enter the number of units of the product I wish to purchase":
-                    multiSearch();
-                    break;
-            }
-        });
-}
-
-function createProduct() {
-    //This function adds a product to the product table in the bamazon database
-    console.log("Inserting a new product ...\n");
-    var query = connection.query(
-        "INSERT INTO products SET ?",
+function purchase(){
+    //Assign the user's input from the product they want to purchase to a variable
+    var intID;
+    var intUnits;
+    var questions = [
         {
-            product_name: "Whisk",              //VARCHAR(45) NULL,
-            department_name: "Kitchen&Bath",    //VARCHAR(45) NULL,
-            price: 7.99,                        //FLOAT (11,2) NULL,
-            stock_quantity: 22                  //INT (11)
+            name: "id_item",
+            type: "input",
+            message: "What is the ID of the item that you would like to purchase?",
+            validate: function(value){
+                if(isNaN(value) === false){
+                    intID = value;
+                    console.log("intID is:  " + intID);
+                    return true;
+                }
+                    return false;
+            }
+        },
+        {
+            name: "units",
+            type: "input", 
+            message:  "How many units of the item would you like to purchase?",
+            validate:  function(value){
+                if(isNaN(value) === false){
+                    intUnits = value;
+                    return true;
+                }
+                return false;
+            }
         }
-    )
-    connection.end();
+    ];
+    inquirer.prompt(questions).then(function(answer){
+        var sqlString = "SELECT item_id, price, stock_quantity FROM products WHERE item_id = " + intID;
+        connection.query(sqlString, intID, function(err, res){
+            if (err) throw err;
+            if(res[0].stock_quantity >= intUnits){
+                //There is sufficient quantity
+                var purchaseTotal = intUnits * res[0].price;
+                var newInventory = res[0].stock_quantity - intUnits;
+                sqlString = "UPDATE products SET stock_quantity = " + newInventory + " WHERE item_id = " + res[0].item_id;
+                connection.query(sqlString, function(error){
+                    if (error) throw error;
+                    //console.log("\nInventory updated successfully");
+                });
+                console.log("\nYour purchase total is:  $" + purchaseTotal);
+            }
+            else{
+                console.log("\nThere is insufficient quantity");
+            }
+            start();
+        });
+    });
 }
+
+// function createProduct() {
+//     //This function adds a product to the product table in the bamazon database
+//     console.log("Inserting a new product ...\n");
+//     var query = connection.query(
+//         "INSERT INTO products SET ?",
+//         {
+//             product_name: "Whisk",              //VARCHAR(45) NULL,
+//             department_name: "Kitchen&Bath",    //VARCHAR(45) NULL,
+//             price: 7.99,                        //FLOAT (11,2) NULL,
+//             stock_quantity: 22                  //INT (11)
+//         }
+//     )
+//     connection.end();
+// }
 
 function displayItems() {
     //This function displays all of the items available for sale. 
@@ -100,5 +127,7 @@ function displayItems() {
             for (var k = 0; k < res.length; k++) {
                 console.log(res[k].Product_ID + ' ' + res[k].Product + ' ' + res[k].Price ); //+ '\n'
             }
+            console.log('\n');
+            console.log('\n');
         });
 }
