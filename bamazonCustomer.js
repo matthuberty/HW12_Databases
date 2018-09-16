a1683fdfaeccb77d323a795881652c6b086af65a
Matthew Huberty <matthew.huberty@gmail.com>
2018-09-16 08:56:23 -0700

    Sept16_v3

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,30 +17,32 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("Connected as ID:  " + connection.threadId + "\n");
+    //console.log("Connected as ID:  " + connection.threadId + "\n");
+    start();
+    displayItems();
     //Call a variety of functions, depending upon what you want to do
     //runSearch();
     //createProduct();
-    start();
+    //start();
 });
 
-function start(){
+function start() {
     var questions = {
-        name: "idOrQuantity", 
+        name: "idOrQuantity",
         type: "rawlist",
-        message: "Would you like to enter an [ID] or [QUANTITY] for an item to purchase?", 
+        message: "Would you like to enter an [ID] or [QUANTITY] for an item to purchase or exit the program? [TYPE 1, 2, OR 3]",
         choices: ["ID", "QUANTITY", "EXIT"]
     };
 
-    inquirer.prompt(questions).then(function(answer){
+    inquirer.prompt(questions).then(function (answer) {
         //based on the user's answer, either enter the ID or quantity of the item they'd like to purchase
-        if(answer.idOrQuantity.toUpperCase() === "ID"){
+        if (answer.idOrQuantity.toUpperCase() === "ID") {
             idAction();
         }
-        else if(answer.idOrQuantity.toUpperCase() === "POST"){
+        else if (answer.idOrQuantity.toUpperCase() === "POST") {
             postAction();
         }
-        else{
+        else {
             //Exit the application
             connection.end();
             return;
@@ -55,7 +57,7 @@ function runSearch() {
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "Enter the ID of the product I wish to purchase", 
+                "Enter the ID of the product I wish to purchase",
                 "Enter the number of units of the product I'd like to buy"
             ]
         })
@@ -72,7 +74,7 @@ function runSearch() {
         });
 }
 
-function createProduct(){
+function createProduct() {
     //This function adds a product to the product table in the bamazon database
     console.log("Inserting a new product ...\n");
     var query = connection.query(
@@ -85,4 +87,18 @@ function createProduct(){
         }
     )
     connection.end();
+}
+
+function displayItems() {
+    //This function displays all of the items available for sale. 
+    //Item aspects include the ids, names, and prices of products for sale.
+    var query = connection.query(
+        "SELECT item_id AS Product_ID, product_name AS Product, price As Price FROM products", function (err, res) {
+            if (err) throw err;
+            //Log all of the results of the SELECT statement
+            console.log('\n');
+            for (var k = 0; k < res.length; k++) {
+                console.log(res[k].Product_ID + ' ' + res[k].Product + ' ' + res[k].Price ); //+ '\n'
+            }
+        });
 }
